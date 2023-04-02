0d8425ddfa9df4a95bdce1c4d7259f8ece71cd85
Sergii Polushkin <seregapola@gmail.com>
2023-04-02 12:04:35 +0200

    feat: start progect

---
+++ app/components/Main.js
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PageScreen from './../screens/PageScreen2';
+
+function HomeScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to the test"
+                onPress={() => navigation.navigate('EPQ-R')}
+            />
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="EPQ-R" component={PageScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
